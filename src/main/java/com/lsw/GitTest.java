ffb410e9926d710156fdff395c757c1d8f11a920
lsw <lsw@qq.com>
2021-04-29 20:41:33 +0800

    git version1

---
+++ src/main/java/com/lsw/GitTest.java
@@ -0,0 +1,11 @@
+package com.lsw;
+
+/**
+ * @author lsw
+ * @create 2021/4/29 20:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
