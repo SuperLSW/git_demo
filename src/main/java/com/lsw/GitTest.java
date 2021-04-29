5a9cd485ed01888e9a2c4b2f1dfdccf76d6e8ea7
lsw <lsw@qq.com>
2021-04-29 20:42:33 +0800

    git version2

--- src/main/java/com/lsw/GitTest.java
+++ src/main/java/com/lsw/GitTest.java
@@ -1,11 +1,16 @@
 package com.lsw;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author lsw
  * @create 2021/4/29 20:39
  */
 public class GitTest {
+    private Map map = new HashMap();
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+
 }
